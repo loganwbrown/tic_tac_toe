311e84b8da92cae9744ceded3b49b457762f75b1
loganwbrown <loganbrown@DPL-4s-iMac.local>
2014-02-12 17:04:03 -0800

    almost done

--- ttt.rb
+++ ttt.rb
@@ -1,9 +1,10 @@
+
 class Board
  
 	def initialize
 		@grid = [[nil,nil,nil],[nil,nil,nil],[nil,nil,nil]]
 	end
- 
+
 	def show
 		puts "	#{@grid[0][0]}	|	#{@grid[0][1]}	|	#{@grid[0][2]}"
 		puts "-------------------------------------------"
@@ -16,17 +17,68 @@ class Board
 		@grid[x][y] = value
 	end	
 
-	def take
-		return 	@grid[0][0] && @grid[0][1] && @grid[0][2] ||
-				@grid[1][0] && @grid[1][1] && @grid[1][2] ||
-				@grid[2][0] && @grid[2][1] && @grid[2][2] ||
-				@grid[0][0] && @grid[1][1] && @grid[2][2] ||
-				@grid[0][0] && @grid[1][0] && @grid[2][0] ||
-				@grid[0][1] && @grid[1][1] && @grid[2][1] ||
-				@grid[0][2] && @grid[1][2] && @grid[2][2] ||
-				@grid[0][2] && @grid[1][1] && @grid[2][0] 
+
+	def winner
+		if 	@grid[0][0] == "X" && @grid[0][1] == "X" && @grid[0][2] == "X"
+			puts "You Win!"
+		elsif
+			@grid[1][0] == "X" && @grid[1][1] == "X" && @grid[1][2] == "X"
+			puts "You Win!"
+		elsif 
+			@grid[2][0] == "X" && @grid[2][1] == "X" && @grid[2][2] == "X"
+			puts "You Win!"
+		elsif 
+			@grid[0][0] == "X" && @grid[1][1] == "X" && @grid[2][2] == "X"
+			puts "You Win!"	
+
+
+		elsif 
+			@grid[0][0] == "X" && @grid[1][0] == "X" && @grid[2][0] == "X"
+			puts "You Win!"	
+		elsif 
+			@grid[0][1] == "X" && @grid[1][1] == "X" && @grid[2][1] == "X"
+			puts "You Win!"	
+		elsif 
+			@grid[0][2] == "X" && @grid[1][2] == "X" && @grid[2][2] == "X"
+			puts "You Win!"	
+		elsif
+			@grid[0][2] == "X" && @grid[1][1] == "X" && @grid[2][0] == "X"
+			puts "You Win!"
+
+
+
+
+
+		elsif
+			@grid[1][0] == "O" && @grid[1][1] == "O" && @grid[1][2] == "O"
+			puts "You Win!"
+		elsif 
+			@grid[2][0] == "O" && @grid[2][1] == "O" && @grid[2][2] == "O"
+			puts "You Win!"
+		elsif 
+			@grid[0][0] == "O" && @grid[1][1] == "O" && @grid[2][2] == "O"
+			puts "You Win!"	
+
+		elsif 
+			@grid[0][0] == "O" && @grid[0][1] == "O" && @grid[0][2] == "O"
+			puts "You Win!"	
+
+		elsif 
+			@grid[0][0] == "O" && @grid[1][0] == "O" && @grid[2][0] == "O"
+			puts "You Win!"	
+		elsif 
+			@grid[0][1] == "O" && @grid[1][1] == "O" && @grid[2][1] == "O"
+			puts "You Win!"	
+		elsif
+			@grid[0][2] == "O" && @grid[1][2] == "O" && @grid[2][2] == "O"
+			puts "You Win!"
+		else 
+			@grid[0][2] == "O" && @grid[1][1] == "O" && @grid[2][0] == "O"
+			puts "You Win!"
+		end
 	end
- 
+
+
 end
  
 class Player
@@ -36,15 +88,6 @@ class Player
 		"unknown player type"
 	end
  
-	# not needed as we are using 
-	# attr_accessor :name
-	#def name
-	#	@name
-	#end
-	#
-	#def name=(value)
-	#	@name = value
-	#end	
 end
  
 class PlayerX < Player
@@ -77,56 +120,36 @@ class Game
 		@player2.name = pear
 
 		@game_in_progress = true
- 
-		def winning
-			if [0][0] != nil
-				@game_in_progress = false
-			else
-				@game_in_progress = true
-			end
-
-		end
-
 
 		game_loop
 
 	end
+
+	
  
 	def game_loop
+
+
 		while @game_in_progress do
-			#ask the current player for input
-			#To ask for input we use gets which returns a string
 			puts "#{@player1.name} make a move ([0-2],[0-2]):"
 			input = gets.chomp
 			x = input.split(",")[0].to_i
 			y = input.split(",")[1].to_i
 			@board.insert(x,y, "X")
 			@board.show
-			if @board.take == "X"
-				puts "#{@player1.name}, you won!"
-			end	
- 
+			@board.winner
+			
 			puts "#{@player2.name} make a move ([0-2],[0-2]):"
 			input = gets.chomp
 			x = input.split(",")[0].to_i
 			y = input.split(",")[1].to_i
 			@board.insert(x,y, "O")
 			@board.show
-			if @board.take == "O"
-				puts "#{@player2.name}, you won!"
-			end	
- 	
- 			
-
-			#update the game based on the input
-			#update the current player		
-		end	
-	end
-
-	def ways_to_win
-		
-	end
+			@board.winner 
+				
+		end
 
 
-end	
 
+end
+end
